0f71c37e8b0c2e4bc74a64e308f841472e77fd5b
Noor ul Amin <noorulamin25@gmail.com>
2016-01-04 13:28:42 +0500

    new xcodeProj added

---
+++ SampleTest/SampleTest/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SampleTest
+//
+//  Created by Asif Noor on 04/01/2016.
+//  Copyright © 2016 Noor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ SampleTest/SampleTest/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  SampleTest
+//
+//  Created by Asif Noor on 04/01/2016.
+//  Copyright © 2016 Noor. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
